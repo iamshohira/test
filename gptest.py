66a5bdc34220f8fb9b6311e4e163ce238a4a30d9
iamshohira <hiraide@cheme.kyoto-u.ac.jp>
2022-08-16 12:53:04 +0900

    test

--- gptest.py
+++ gptest.py
@@ -1,7 +1,8 @@
 import git
 import sys,os
 repo = git.Repo(os.path.dirname(sys.argv[0]))
+print(next((tag for tag in repo.tags if tag.commit == repo.head.commit), None))
 origin = repo.remote()
 origin.fetch()
-tag = repo.git.tag()
+tag = repo.git.tag().split("\n")
 print(tag)
